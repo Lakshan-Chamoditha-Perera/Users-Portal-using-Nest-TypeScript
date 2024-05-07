c523dbec7e069f17fb74c6746891a113190dbd3b
Lakshan Chamoditha <chamodithap@ceyentra.com>
2024-05-07 19:09:45 +0530

    handling routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,13 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
     constructor() {}
+
+    @Get()
+    findAllByQuery(@Query('role' )role?:'INTERNS' | 'EMPLOYEES'){
+        return 'This action returns all users';
+    }
 
     @Get()
     findAll(): string {
@@ -27,6 +32,11 @@ export class UsersController {
     @Patch(':id')
     update(@Param('id') id: string, @Body() user:{}): string{
         return `This action updates a ${id} user : ` + JSON.stringify(user);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string): string {
+        return `This action removes a #${id} user`;
     }
 
 }
