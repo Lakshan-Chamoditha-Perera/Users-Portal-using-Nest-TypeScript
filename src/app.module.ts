17e422e36833fbf34a04c1d1afbd661b14239dc1
Lakshan Chamoditha <chamodithap@ceyentra.com>
2024-05-08 14:28:25 +0530

    employee crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { DatabaseModule } from './database/database.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UsersModule} from './users/users.module';
+import {DatabaseModule} from './database/database.module';
+import {EmployeesModule} from "./employees/employees.module";
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UsersModule, DatabaseModule, EmployeesModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
